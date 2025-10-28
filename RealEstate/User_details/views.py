60e7b5a4df96903a66dfb3f6b5bf7ce14329da3e
dycy21 <openmevi@gmail.com>
2025-10-28 15:43:27 +0530

    error fixed in views.py

--- RealEstate/User_details/views.py
+++ RealEstate/User_details/views.py
@@ -1,16 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from .serializers import UserRegistrationSerializer
+# Import the new serializer
+from .serializers import UserRegistrationSerializer, CustomTokenObtainPairSerializer
 from .models import CustomUser
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Custom Email-based Login View
 class CustomTokenObtainPairView(TokenObtainPairView):
-    # We use the default serializer, but tell it to use 'email'
-    username_field = 'email' 
+    # Tell this view to use your new custom serializer
+    serializer_class = CustomTokenObtainPairSerializer
 
 # User Registration View
 class UserRegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = UserRegistrationSerializer
+
