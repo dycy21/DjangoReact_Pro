ed2e2565e0cfe408260c18fc4d96f571e755bbcb
dycy21 <openmevi@gmail.com>
2025-10-28 18:53:08 +0530

    some fixes in property url

--- RealEstate/property_details/urls.py
+++ RealEstate/property_details/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropertyViewSet, GenerateCloudinarySignatureView
+# --- FIX ---
+# We are commenting out GenerateCloudinarySignatureView because it is
+# currently commented out in your views.py file and causing a crash.
+from .views import PropertyViewSet #, GenerateCloudinarySignatureView
 
 # Create a router and register our viewset with it.
 router = DefaultRouter()
@@ -8,8 +11,9 @@ router.register(r'properties', PropertyViewSet, basename='property')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    # /api/v1/generate-upload-signature/
-    path('generate-upload-signature/', GenerateCloudinarySignatureView.as_view(), name='generate-upload-signature'),
+    # --- FIX ---
+    # Commenting out the path for the view we just removed.
+    # path('generate-upload-signature/', GenerateCloudinarySignatureView.as_view(), name='generate-upload-signature'),
     
     # /api/v1/... (includes /properties/, /properties/<id>/, etc.)
     path('', include(router.urls)),
